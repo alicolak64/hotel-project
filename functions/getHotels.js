8618d547f9bedc7c5e591ad934140fec35cba617
alicolak64 <20190808064@ogr.akdeniz.edu.tr>
2022-08-25 16:24:35 +0300

    added getHotels

---
+++ functions/getHotels.js
@@ -0,0 +1,47 @@
+const fetch = require('node-fetch');
+
+
+exports.handler = async function (event) {
+
+    const query = `
+        query {
+            hotelData {
+                values {
+                    id ,
+                    name ,
+                    rating
+                }
+            }
+        }
+    `
+
+
+    const url = process.env.GETHOTELS
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-cassandra-token': process.env.X_Cassandra_Token
+        } ,
+        body: JSON.stringify({ query })
+    }
+
+    const response = await fetch(url, options)
+
+
+    try {
+        const responseBody = await response.json()
+        return {
+            statusCode: 200,
+            body: JSON.stringify(responseBody)
+        }
+    } catch (error) {
+        console.log(error)
+        return {
+            statusCode: 500,
+            body: JSON.stringify(error)
+        }
+    }
+
+}
+
